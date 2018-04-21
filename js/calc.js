4ecc1c38f1adec4c5e76e5dad2475bc1f68e73db
JSENL <jasenlambright@gmail.com>
2018-04-21 14:00:52 -0400

    Got numbers show up yall!

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,15 @@
 /* Stubs */
+/*
+prompt("38479aq7848hsdfdjskn /.")*/
+
+
+
+function showNumber(value){
+	document.getElementById("d").value = value
+
+}
+
+function myFunction() {
+    var numPresent = document.querySelector("#d").innerHTML;
+    console.log(numPresent);
+}
